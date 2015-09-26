fca2e30c27d33e71fcce66fe54e01f78438a0ba9
byendan <byendanb@gmail.com>
2015-09-25 21:29:55 -0700

    Graphics resize when window changes size

--- main.js
+++ main.js
@@ -13,19 +13,30 @@ function main() {
     var greenBlockHeight = 50;
     
 
-    // green rect
-    draw.rect(display, '#46ac41', new gamejs.Rect([0, canHeight - greenBlockHeight], [canWidth, greenBlockHeight]), 0);
+    
     
 
     gamejs.event.onEvent(function(event) {
         // event handling
+        gamejs.event.onDisplayResize(function() {
+            var canHeight = $('#gjs-canvas').height();
+            var canWidth = $('#gjs-canvas').width();
+            draw.rect(display, '#46ac41', new gamejs.Rect([0, canHeight - greenBlockHeight], [canWidth, greenBlockHeight]), 0);
+        });
     });
     // there are also more special functions
     // gamejs.event.onKeyUp, gamejs.event.onMouseMotion,...
+    
+
 
     gamejs.onTick(function(msDuration) {
         // game loop
+        // green rect
+    draw.rect(display, '#46ac41', new gamejs.Rect([0, canHeight - greenBlockHeight], [canWidth, greenBlockHeight]), 0);
+
     });
+    
+    
 };
 
 gamejs.ready(main);
