d24614500f62708ab9b1dc8653e30b57fbf759bc
byendan <byendanb@gmail.com>
2015-09-26 18:29:12 -0700

    Added a panda that jumps. Changes to canvas dimentions

--- main.js
+++ main.js
@@ -2,20 +2,25 @@ var gamejs = require('gamejs');
 var draw = require('gamejs/graphics');
 var font = require('gamejs/font');
 
-gamejs.preload([]);
+
+
 
 function main() {
 
     var display = gamejs.display.getSurface();
+    var staticPanda = gamejs.image.load('img/mikos/static1.png');
+    staticPanda.scale([20, 2]);
     
     var canHeight = $('canvas').height();
     var canWidth = $('canvas').width();
-    var greenBlockHeight = 50;
+    var greenBlockHeight = 25;
     var score = 0;
     var alive = false;
     var start = false;
-    
-
+    var jumpUp = false;
+    var jumpDown = false;
+    var pandaHeight = 0;
+    var pandaY = 0;
     
     
 
@@ -35,6 +40,9 @@ function main() {
                }
            } else if (event.key === gamejs.event.K_j) {
             // Jump method   
+            if(!jumpUp && !jumpDown){
+                jumpUp = true;
+            }
            } else if (event.key === gamejs.event.K_p) {
             // Punch method   
            }
@@ -47,13 +55,28 @@ function main() {
 
 
     gamejs.onTick(function(msDuration) {
-        // game loop
-        // green rect
-    draw.rect(display, '#46ac41', new gamejs.Rect([0, canHeight - greenBlockHeight], [canWidth, greenBlockHeight]), 0);
-
+        display.fill('#ffffff');
+        if(jumpUp && pandaHeight < 105) {
+            pandaHeight += 7;
+            if(pandaHeight === 105){
+                jumpUp = false;
+                jumpDown = true;
+            }
+        } else if(jumpDown && pandaHeight > 0){
+            pandaHeight -= 7;
+            if(pandaHeight === 0){
+                jumpDown = false;   
+            }
+        }
+        
+        pandaY = canHeight - greenBlockHeight - staticPanda.getSize()[1] - pandaHeight;
+        
+        draw.rect(display, '#46ac41', new gamejs.Rect([0, canHeight - greenBlockHeight], [canWidth, greenBlockHeight]), 0);
+        display.blit(staticPanda, [25, pandaY]);
     });
     
     
 };
 
+gamejs.preload(['img/mikos/hit1.png', 'img/mikos/hit2.png', 'img/mikos/jump1.png', 'img/mikos/jump2.png', 'img/mikos/jump3.png', 'img/mikos/portrait.png', 'img/mikos/static1.png', 'img/mikos/walk1.png', 'img/mikos/walk2.png', 'img/mikos/walk3.png', 'img/mikos/walk4.png']);
 gamejs.ready(main);
